b2959c9cb04c5de5d328bcbacd6532d67299f22a
jorge-73 <jorge_ap_73@hotmail.com>
2023-01-01 23:51:48 -0300

    PreEntrega3+PeraltaJorge

--- index.js
+++ index.js
@@ -1,46 +1,50 @@
-let numeroCarrito = document.getElementById('numeroCarrito');
-let container = document.getElementById('productos');
-let vaciarCarrito = document.getElementById('vaciarCarrito');
-let bodyModal = document.getElementById('modalBody');
-let btnModal = document.getElementById('btnMostrar');
-let form = document.getElementById('form');
-let modalAlert = document.getElementById('modalAlert');
-let precioTotal = document.getElementById('precioTotal');
-
-let productosStorage;
-
-document.addEventListener('DOMContentLoaded', () => {
-    productosStorage = JSON.parse(localStorage.getItem("productosStorage")) || [];
-})
-
 // Creación del constructor para los productos de ejemplo que se mostraran como propaganda
 class Productos{
-    constructor(nombre, descripcion, precio, img){
+    constructor(id, nombre, descripcion, precio, cantidad, img){
+        this.id = id;
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.precio = precio;
+        this.cantidad = cantidad;
         this.imagen = img;
     }
 }
 // Lista de los objetos/productos creados mientras se lo va guardando en un array
 const listaProductos = [];
-listaProductos.push(new Productos('Celular Samsung', 'Modelo A22', 122000, "img/samsungA22.jpg"));
-listaProductos.push(new Productos('Celular lg', 'Modelo K20', 72000, "img/lgK20.jpg"));
-listaProductos.push(new Productos('Celular Motorola', 'Modelo G30', 102000, "img/motorolaG30.jpg"));
-listaProductos.push(new Productos('Celular Samsung', 'Modelo S21', 169999, "img/samsungS21.jpg"));
-listaProductos.push(new Productos('Celular Motorola', 'Modelo Edge 30', 119999, "img/motorolaEdge30.jpg"));
-listaProductos.push(new Productos('Celular Samsung', 'Modelo S20', 159999, "img/samsungS20.jpg"));
-listaProductos.push(new Productos('Celular Apple', 'iPhone SE', 317499, "img/appleiPhoneSE.jpg"));
-listaProductos.push(new Productos('Celular Apple', 'iPhone 14 Pro Max', 837000, "img/appleiPhone14ProMax.jpg"));
-listaProductos.push(new Productos('Celular Sony', 'Xperia 5 lv', 300000, "img/SonyXperia5.jpg"));
-listaProductos.push(new Productos('Celular Xiaomi', 'Redmi Note 9', 154800, "img/xiaomiRedmiNote9.jpg"));
+listaProductos.push(new Productos(1, 'Celular Samsung', 'Modelo A22', 122000, 1, "img/samsungA22.jpg"));
+listaProductos.push(new Productos(2, 'Celular lg', 'Modelo K20', 72000, 1, "img/lgK20.jpg"));
+listaProductos.push(new Productos(3, 'Celular Motorola', 'Modelo G30', 102000, 1, "img/motorolaG30.jpg"));
+listaProductos.push(new Productos(4, 'Celular Samsung', 'Modelo S21', 169999, 1, "img/samsungS21.jpg"));
+listaProductos.push(new Productos(5, 'Celular Motorola', 'Modelo Edge 30', 119999, 1, "img/motorolaEdge30.jpg"));
+listaProductos.push(new Productos(6, 'Celular Samsung', 'Modelo S20', 159999, 1, "img/samsungS20.jpg"));
+listaProductos.push(new Productos(7, 'Celular Apple', 'iPhone SE', 317499, 1, "img/appleiPhoneSE.jpg"));
+listaProductos.push(new Productos(8, 'Celular Apple', 'iPhone 14 Pro Max', 837000, 1, "img/appleiPhone14ProMax.jpg"));
+listaProductos.push(new Productos(9, 'Celular Sony', 'Xperia 5 lv', 300000, 1, "img/SonyXperia5.jpg"));
+listaProductos.push(new Productos(10, 'Celular Xiaomi', 'Redmi Note 9', 154800, 1, "img/xiaomiRedmiNote9.jpg"));
+
+let carritoStorage = [];
+
+let numeroCarrito = document.getElementById('numeroCarrito');
+let contenedorProductos = document.getElementById('productos');
+let vaciarCarrito = document.getElementById('vaciarCarrito');
+let btnModal = document.getElementById('btnMostrar');
+let form = document.getElementById('form');
+let modalAlert = document.getElementById('modalAlert');
+let precioTotal = document.getElementById('precioTotal');
+let alerta = document.getElementById('sectionAlert');
+
+document.addEventListener('DOMContentLoaded', ()=> { 
+    carritoStorage = JSON.parse(localStorage.getItem("carritoStorage")) || [];
+    imprimirCarrito();
+})
 
 const funcionImprime = (e) => {
+
     e.preventDefault();
     let formulario = e.target.children;
     let divisas = formulario[0].children[1].value;
     let resNumber = formulario[1].children[1].value;
-
+    
     switch (divisas) {
         case 'dolar':
             totalCambio = Math.fround(resNumber * 315);
@@ -55,184 +59,172 @@ const funcionImprime = (e) => {
             totalCambio = Math.fround(resNumber * 62.60);
         break;
     }
-
+    
     if (totalCambio) {
         result = document.getElementById('result');
         result.innerHTML = `AR$${totalCambio}`; 
     }
 
-    if (document.getElementById('bienvenida')) {
-        bienvenida = document.getElementById('bienvenida');
-        // bucle para mostrar los productos mientras que el valor retornado alcance para comprar algunos de ellos
-        for(const producto of listaProductos) {
-            if (totalCambio > producto.precio) {
-                bienvenida.remove();
-                let card = document.createElement('div');
-                card.innerHTML = `
-                    <div class="card text-center shadow p-1 card_prod">
-                        <img src=${producto.imagen} class="img_prod" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">${producto.nombre}</h5>
-                            <h6 class="card-text">${producto.descripcion}</h6>
-                        </div>
-                        <div class="card-footer d-flex justify-content-evenly">
-                            <h5 class='mt-1'>$<span id="precioProd">${producto.precio}</span></h5>
-                            <button class="btn btn-primary rounded carrito"><img src="./img/carrito-de-compras.png" class="img-carrito" alt="carrito"></button>
-                        </div>
-                    </div>`;
-                container.append(card);
-            }
-        }
-        agregarAlCarrito();
+    ImprimirPantallaConProductos();
 
-    }else {
-        let idCard = document.querySelectorAll(".card_prod");
-        idCard.forEach(carta => {
-        carta.parentElement.remove(); });
-
-        // bucle para mostrar los productos mientras que el valor retornado alcance para comprar algunos de ellos
-        for(const producto of listaProductos) {
-            if (totalCambio > producto.precio) {
-                let card = document.createElement('div');
-                card.innerHTML = `
-                        <div class="card text-center shadow p-1 card_prod">
-                            <img src=${producto.imagen} class="img_prod" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">${producto.nombre}</h5>
-                                <h6 class="card-text">${producto.descripcion}</h6>
-                            </div>
-                            <div class="card-footer d-flex justify-content-evenly">
-                                <h5 class='mt-1'>$<span id="precioProd">${producto.precio}</span></h5>
-                                <button class="btn btn-primary rounded carrito"><img src="./img/carrito-de-compras.png" class="img-carrito" alt="carrito"></button>
-                            </div>
-                        </div>`;
-                container.append(card);
-            }
-        }
-        agregarAlCarrito();
-    }
 };
 form.addEventListener('submit', funcionImprime);
 
-// Funcion para agregar el producto al carrito
-const agregarAlCarrito = ()=>{
-    if (document.querySelectorAll("img[class='img-carrito']")) {
 
-        let alerta = document.getElementById('sectionAlert');
-        let carrito = document.querySelectorAll("img[class='img-carrito']");
-        let nuevosProductos;
-    
-        for (let carro of carrito) {
-            carro.addEventListener('click', ()=>{
-
-                let carProducto = {
-                    carImg: carro.offsetParent.children[0].src,
-                    carNombre: carro.offsetParent.children[1].children[0].innerText,
-                    carDescripcion: carro.offsetParent.children[1].children[1].innerText,
-                    carPrecio: carro.offsetParent.children[2].children[0].children[0].innerText };
-                    console.log(carProducto.carPrecio);
-                let productosStorage = JSON.parse(localStorage.getItem("productosStorage"));
-
-                productosStorage ? nuevosProductos = productosStorage : nuevosProductos = [];
-
-                nuevosProductos.push(carProducto);
-
-                localStorage.setItem("productosStorage", JSON.stringify(nuevosProductos));
-
-                // mensaje en caso de que se cargue los items al carrito
-                alerta.innerHTML = `
-                    <div class="alert alert-success alert-dismissible fade show mt-5 rounded-3" role="alert">
-                    <h5>Agregado al Carrito correctamente.!</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>`;
-                setTimeout(() => {
-                    alerta.innerHTML= "";
-                }, 3000);
-            });
-        };
+function ImprimirPantallaConProductos() {
+    if (document.getElementById('bienvenida')) {
+        bienvenida = document.getElementById('bienvenida');
+        // bucle para mostrar los productos mientras que el valor retornado alcance para comprar algunos de ellos
+        listaProductos.forEach((producto)=>{
+            const {id, nombre, descripcion, precio, imagen} = producto;
+            if (totalCambio > precio) {
+                bienvenida.remove();
+                 let card = document.createElement('div');
+                         card.innerHTML += `
+                             <div class="card text-center shadow p-1 card_prod">
+                                 <img src=${imagen} class="img_prod" alt="...">
+                                 <div class="card-body">
+                                     <h5 class="card-title">${nombre}</h5>
+                                     <h6 class="card-text">${descripcion}</h6>
+                                 </div>
+                                 <div class="card-footer d-flex justify-content-evenly">
+                                     <h5 class='mt-1'>$<span id="precioProd">${precio}</span></h5>
+                                     <button onclick="agregarAlCarrito(${id})" class="btn btn-primary rounded carrito"><img src="./img/carrito-de-compras.png" class="img-carrito" alt="carrito"></button>
+                                 </div>
+                             </div>`;
+                 contenedorProductos.append(card);
+             }
+         })
+         imprimirCarrito();
+     }else {
+        let idCard = document.querySelectorAll(".card_prod");
+        idCard.forEach(carta => {
+        carta.parentElement.remove(); });
+        // bucle para mostrar los productos mientras que el valor retornado alcance para comprar algunos de ellos
+        listaProductos.forEach((producto)=>{
+            const {id, nombre, descripcion, precio, imagen} = producto;
+            if (totalCambio > precio) {
+                 let card = document.createElement('div');
+                         card.innerHTML += `
+                             <div class="card text-center shadow p-1 card_prod">
+                                 <img src=${imagen} class="img_prod" alt="...">
+                                 <div class="card-body">
+                                     <h5 class="card-title">${nombre}</h5>
+                                     <h6 class="card-text">${descripcion}</h6>
+                                 </div>
+                                 <div class="card-footer d-flex justify-content-evenly">
+                                     <h5 class='mt-1'>$<span id="precioProd">${precio}</span></h5>
+                                     <button onclick="agregarAlCarrito(${id})" class="btn btn-primary rounded carrito"><img src="./img/carrito-de-compras.png" class="img-carrito" alt="carrito"></button>
+                                 </div>
+                             </div>`;
+                 contenedorProductos.append(card);
+             }
+         })
+        imprimirCarrito();
     }
 }
 
-// Boton para eliminar todos los elementos del carrito/localStorage
-vaciarCarrito.addEventListener('click', ()=>{
-    if (JSON.parse(localStorage.getItem("productosStorage"))) {
-        localStorage.clear();
+// Funcion para agregar el producto al carrito
+function agregarAlCarrito(id){
 
+    const prodExiste = carritoStorage.some(prod => prod.id === id);
+
+    if (prodExiste) {
+        carritoStorage.map(prod => {
+            if (prod.id === id) {
+                prod.cantidad++;
+            }
+        })
+    }else {
+        const item = listaProductos.find((producto) => producto.id === id);
+        item.cantidad = 1;
+        carritoStorage.push(item);
+    }
+
+    // mensaje en caso de que se cargue los items al carrito
+    alerta.innerHTML = `
+    <div class="alert alert-success alert-dismissible fade show mt-5 rounded-3" role="alert">
+    <h5>Agregado al Carrito correctamente.!</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>`;
+    setTimeout(() => {
+        alerta.innerHTML= "";
+    }, 3000);
+
+    imprimirCarrito();
+}
+
+const imprimirCarrito = () => {
+    let modalBody = document.querySelector('.modal #modalBody');
+    modalBody.innerHTML = '';
+    carritoStorage.forEach((carProducto) => {
+        const {id, nombre, descripcion, precio, cantidad, imagen} = carProducto;
+                modalBody.innerHTML += `
+                    <tr>
+                    <td><h5>${nombre}</h5></td>
+                    <td><h5>${descripcion}</h5></td>
+                    <td><img src="${imagen}" alt="" class="img-modal"></td>
+                    <td><h5>${precio}</h5></td>
+                    <td><h5>${cantidad}</h5></td>
+                    <td><button onclick="eliminarProducto(${id})" class="btn btn-danger rounded-5 btnDelete"><img src="./img/bote-de-basura.png" class="img-modal-delete"></button></td>
+                    </tr>`;          
+    })
+
+    // Mensaje Mostrado si el carrito se encuentra Vacio.
+    if (carritoStorage.length == 0) {
+        modalBody.innerHTML = `
+            <tr>
+                <td colspan="6"><h5 class="text-primary mt-1">Carrito Vacio.!</h5></td>
+            </tr>`;
+    }
+
+    // Imprimiendo cantidad de items del Carrito
+    numeroCarrito.textContent = carritoStorage.reduce((acc, producto) => acc + producto.cantidad, 0);
+
+    // Sumando todos los precios de los productos
+    precioTotal.innerText = carritoStorage.reduce((acc, producto) => acc + producto.cantidad * producto.precio, 0);
+
+    guardarStorage();
+
+}
+// Borrando un unico item del Carrito
+function eliminarProducto(id) {
+    if (carritoStorage.some(prod => prod.id === id)) {
+     
+        carritoStorage.map(prod => {
+            if (prod.id === id) {
+                    prod.cantidad--;
+                }
+        })
+        
+        if (carritoStorage.some(prod => prod.cantidad < 1)) {
+            const prodID = id;
+            carritoStorage = carritoStorage.filter((prod) => prod.id !== prodID);
+        }
+    }
+    imprimirCarrito();
+}
+
+// Guardar productos del carrito en el LocalStorage
+function guardarStorage(){
+    localStorage.setItem("carritoStorage", JSON.stringify(carritoStorage));
+}
+
+// Boton para eliminar todos los elementos del carrito/localStorage
+vaciarCarrito.addEventListener('click', () => {
+    if (carritoStorage.length != 0) {
+        carritoStorage.length = [];
         Swal.fire(
             'Exito!',
             'Carrito vaciado Correctamente!',
             'success'
-          )
-        
+            )
     }else{
         Swal.fire({
             icon: 'error',
             title: 'Error...',
             text: 'Ingrese productos al carrito!'
-          })
-    }
-})
-
-// Modal Bootstrap
-const myModal = new bootstrap.Modal(document.getElementById('modalId'));
-
-    // Imprimiendo en una tabla dentro del modal los productos que se agregan al localStorage
-    if (localStorage.hasOwnProperty("productosStorage")) {
-        let productStorage = JSON.parse(localStorage.getItem("productosStorage"));
-        let mostrarCarrito = () => {
-            for(const prodST of productStorage) {
-                let trModal = document.createElement('tr');
-                    trModal.innerHTML = `
-                        <td><h5>${prodST.carNombre}</h5></td>
-                        <td><h5>${prodST.carDescripcion}</h5></td>
-                        <td><img src="${prodST.carImg}" alt="" class="img-modal"></td>
-                        <td><h5>${prodST.carPrecio}</h5></td>
-                        <td><button class="btn btn-danger rounded-4 btnDelete"><img src="./img/bote-de-basura.png" class="img-modal"></button></td>`;
-            bodyModal.append(trModal);
-            };
-        }
-        productStorage ? mostrarCarrito() : bodyModal.append(`<H5>Carrito Vacío</H5>`);
-    }
-    // Imprimiendo cantidad de items del Carrito
-    if (JSON.parse(localStorage.getItem("productosStorage"))) {
-        numeroCarrito.textContent = (JSON.parse(localStorage.getItem("productosStorage"))).length;
-    }
-
-    // Borrando un unico item del Carrito
-    if (document.getElementsByClassName('btnDelete')) {
-        let btnDelete = document.getElementsByClassName('btnDelete');
-        btnDelete = Array.from(btnDelete);
-        btnDelete.forEach((btn)=>{
-            btn.addEventListener('click', ()=>{
-                let del = btn.parentElement.parentElement.children[1].innerText;
-                let rowDelete = JSON.parse(localStorage.getItem("productosStorage"));
-                let borrado = rowDelete.find(dele=> dele.carDescripcion === del);
-                newRow = rowDelete.filter(deleteStorage => deleteStorage !== borrado);
-                localStorage.setItem("productosStorage", JSON.stringify(newRow));
-
-                // mensaje en caso de que se elimine un item del carrito
-                modalAlert.innerHTML = `
-                    <div class="alert alert-danger alert-dismissible fade show mt-5 rounded-3" role="alert">
-                    <h5>Producto Eliminado.!</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>`;
-                setTimeout(() => {
-                    modalAlert.innerHTML= "";
-                }, 3000);
-            });
         })
     }
-
-    // Sumando todos los precios de los productos
-    console.log(precioTotal.innerText);
-    let rowPrecio = JSON.parse(localStorage.getItem("productosStorage"));
-    let bm = bodyModal.children;
-    bm = Array.from(bm);
-    rowPrecio = [];
-    bm.forEach((row)=>{
-        rowPrecio.push(parseInt(row.children[3].children[0].innerText));
-        let total = rowPrecio.reduce((a,b) => a + b, 0);
-        precioTotal.innerText = total;
-        
-    })
-   
+    imprimirCarrito();
+})
